8d3012c2aa6676fd27394a85770da7837014544d
saucecodee <ashawinta@gmail.com>
2022-09-23 10:05:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require("cors")
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'data')))
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`)
+})
