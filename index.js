753528e81e2dceded5ef64004b83b168109adbf0
Okafor Obinna Kingsley <kingsloob1@gmail.com>
2022-09-23 10:35:58 +0100

    Change default port to 3001

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require("cors")
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(morgan('dev'));
