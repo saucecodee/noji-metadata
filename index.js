ea2e1eaac00d019c21c2db1686cdd9b58537aace
Okafor Obinna Kingsley <kingsloob1@gmail.com>
2022-09-23 10:37:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'data')))
 
+app.get('/', (req, res) => {
+  res.send('Hello World in meta.noji.so!')
+})
+
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`)
 })
